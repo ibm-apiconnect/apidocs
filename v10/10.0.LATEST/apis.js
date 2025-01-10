657718af5e8f5784fc3679879990a59f5cbaa52b
Daniel Annan <daniel.annan@ibm.com>
2025-01-10 16:20:42 +0000

    Fix v10.0.9.0

--- v10/10.0.LATEST/apis.js
+++ v10/10.0.LATEST/apis.js
@@ -48,7 +48,6 @@
         "10.0.8.1": "10.0.8.1.html",
         "10.0.8.LATEST": "10.0.8.LATEST.html",
         "10.0.9.0": "10.0.9.0.html",
-        "10.0.9.LATEST": "10.0.9.LATEST.html",
         "10.0.LATEST": "10.0.LATEST.html",
       }
     },
